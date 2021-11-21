6c72b134b1136ac4afdc6988b570a414bdc31cfe
Rebecca Zee <ryz283@gmail.com>
2021-11-21 01:39:07 -0500

    18. Parse JSON response in Quake Report app

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,15 +31,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake(6.1, "London", "July 20, 2015"));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", "November 10, 2014"));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3, 2014"));
-        earthquakes.add(new Earthquake(2.8, "Moscow", "January 31, 2013"));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "August 19, 2012"));
-        earthquakes.add(new Earthquake(1.6, "Paris", "October 30, 2011"));
+        // Create a fake list of earthquakes.
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = findViewById(R.id.list);
